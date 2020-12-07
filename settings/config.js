3e5468b32c35e54a571622f4fad93f5dd2e20c66
vova123298 <52409407+vova123298@users.noreply.github.com>
2020-12-07 18:07:24 +0300

    1
    1

---
+++ settings/config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+	codename: "TEAMMS|BOT",//Кодовое имя
+	namebot: "TEAMMS|BOT", //Имя бота
+	version: "3.1", //Версия кода
+	admin: "[direnyov|Даниил]", //2 руководитель бота
+	coder: "[direnyov|Даниил]", //Для доната
+	zam: "Не найден", //Заместитель
+	site: "В группе", //Сайт, если есть
+	group_url: "[botteamms|TEAMMS|BOT]",//12213
+}
